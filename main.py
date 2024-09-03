420ac8aae9c1b441496e0f6d0194446073c2f091
Neto6391 <neto6391@gmail.com>
2024-09-03 17:14:27 -0300

    feat: send project

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+from customer_info import coletar_informacoes_cliente
+from pet_info import coletar_informacoes_pet
+
+def main():
+    coletar_informacoes_cliente()
+    coletar_informacoes_pet()
+
+if __name__ == "__main__":
+    main()
